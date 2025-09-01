4a2cbdb8c9223ee8cb7a9ee1bc4e3e34700e6dd3
kody <zj.wang.work@zohomail.cn>
2025-09-01 11:08:44 +0800

    feat: 添加路由导航和页面组件
    - 在 App.vue 中添加了路由导航链接和路由视图
    - 新增 router/index.ts 文件，定义了应用的路由配置
    - 创建了 About、Home 和 Login 页面组件
    - 在 main.ts 中集成了路由和 ElementPlus 组件库
    - 更新了 package.json，添加了相关依赖

---
+++ src/views/About/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<p>Hello About !!!</p>
+</template>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<p>Hello Home</p>
+</template>
+
+<style scoped>
+
+</style>
